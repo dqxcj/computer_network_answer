7ef8c6cac961e0edc69a5664840c9461b621a9d2
ljy.sj <ljy.sj@qq.com>
2022-08-14 20:12:31 +0800

    课后题

--- chapter2/UDPClient.py
+++ chapter2/UDPClient.py
@@ -3,6 +3,7 @@ from socket import *
 serverName = '192.168.56.102'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.bind(('', 5432))
 while True:
     message = input('me> ')
     if(message == "quit"):
